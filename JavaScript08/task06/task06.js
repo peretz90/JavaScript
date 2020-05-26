12cb7de3021ec949a760ec69f882591f23bd2730
peretz90 <peretz90@mail.ru>
2020-05-26 16:04:22 +0300

    by peretz90

--- JavaScript08/task06/task06.js
+++ JavaScript08/task06/task06.js
@@ -1,19 +1,22 @@
 let direction = document.getElementById('direction');
 let search = document.getElementById('search');
 let info = document.getElementById('info');
-let places = new Map([[1, ''],[2, ''],[3, ''],[4, ''],[5, ''],[6, ''],[7, ''],[8, ''],[9, ''],[10, ''],[11, ''],[12, ''],[13, ''],[14, ''],[15, ''],[16, ''],[17, ''],[18, ''],[19, ''],[20, ''],[21, ''],[22, ''],[23, ''],[24, ''],[25, ''],[26, ''],[27, ''],[28, '']]);
+const PLACES = new Map([[1, ''],[2, ''],[3, ''],[4, ''],[5, ''],[6, ''],[7, ''],[8, ''],[9, ''],[10, ''],[11, ''],[12, ''],[13, ''],[14, ''],[15, ''],[16, ''],[17, ''],[18, ''],[19, ''],[20, ''],[21, ''],[22, ''],[23, ''],[24, ''],[25, ''],[26, ''],[27, ''],[28, '']]);
 let saved = new Map();
 let date = document.getElementsByTagName('input')[0];
+let modal = document.getElementById('modal');
+let textModal = document.getElementById('text_modal');
+let closeModal = document.getElementById('close_modal');
 
 function createInfo(map) {
-  let string = `<form><fieldset><legend>Забронировать место</legend><table><tr>`;
+  let string = `<form><fieldset><legend>Забронировать место</legend><table id="places"><tr>`;
   for (let num of map.keys()) {
     if (num % 2 !== 0) {
       string += `<td><input type="checkbox" ${map.get(num) === 'true' ? 'disabled checked' : ''} value="${num}"> ${num}</td>`;
     }
   }
   string += `</tr><tr>`;
-  for (let num of places.keys()) {
+  for (let num of map.keys()) {
     if (num % 2 === 0) {
       string += `<td><input type="checkbox" ${map.get(num) === 'true' ? 'disabled checked' : ''} value="${num}"> ${num}</td>`;
     }
@@ -24,10 +27,11 @@ function createInfo(map) {
 }
 
 search.onclick = function() {
-  if (saved.has(date.value)) {
-    createInfo(saved.get(date.value));
+  info.style.display = 'block';
+  if (saved.has(date.value + ' ' + direction.value)) {
+    createInfo(saved.get(date.value + ' ' + direction.value));
   } else {
-    createInfo(places);
+    createInfo(PLACES);
   }
   for (let i of document.getElementsByTagName('input')) {
     if (i.type === 'checkbox') {
@@ -40,7 +44,7 @@ function onChecked() {
   let sum = 0;
   let input = document.getElementsByTagName('input');
   for (let i of input) {
-    if (i.checked) {
+    if (i.checked && !i.disabled) {
       sum += direction.value ^ 0;
     }
   }
@@ -48,12 +52,29 @@ function onChecked() {
 }
 
 function buy() {
-  let reserve = places;
+  let str = `<tr><th><b>Direction</b></th><th><b>Date</b></th><th><b>Seat</b></th></tr>`;
+  let reserve = new Map([[1, ''],[2, ''],[3, ''],[4, ''],[5, ''],[6, ''],[7, ''],[8, ''],[9, ''],[10, ''],[11, ''],[12, ''],[13, ''],[14, ''],[15, ''],[16, ''],[17, ''],[18, ''],[19, ''],[20, ''],[21, ''],[22, ''],[23, ''],[24, ''],[25, ''],[26, ''],[27, ''],[28, '']]);
   let input = document.getElementsByTagName('input');
+  let isTickets = false;
   for (let i of input) {
     if (i.checked) {
       reserve.set(i.value ^ 0, 'true');
+      if (!i.disabled) {
+        isTickets = true;
+        str += `<tr><td>${direction.options[direction.selectedIndex].text}</td><td>${date.value}</td><td>${i.value}</td></tr>`;
+      }
     }
   }
-  saved.set(date.value, reserve);
+  saved.set((date.value + ' ' + direction.value), reserve);
+  modal.style.display = 'block';
+  if (isTickets) {
+    textModal.innerHTML = str;
+  } else {
+    textModal.innerHTML = `<caption>Билеты не выбраны</caption>`;
+  }
+}
+
+closeModal.onclick = function() {
+  modal.style.display = 'none';
+  info.style.display = 'none';
 }
